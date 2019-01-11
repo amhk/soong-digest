f6d1a68e616ff30c3d4d7d690f816648d07ac678
Mårten Kongstad <marten.kongstad@gmail.com>
2019-01-11 16:03:53 +0100

    Add integration test to verify tests/**/verbose.log.gz can be parsed
    Because the warning parser is so slow, mark the test as #[ignore]. To
    run it, use
    
      $ cargo test -- --ignored
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- tests/integration.rs
+++ tests/integration.rs
@@ -27,3 +27,25 @@ fn test_parse_all_error_data() {
     let o5 = exec("--errors=tests/data/idmap-errors/error.log");
     assert!(o5.status.success());
 }
+
+#[test]
+#[ignore]
+fn test_parse_all_warning_data() {
+    let o0 = exec("--warnings=this-does-not-exist");
+    assert!(!o0.status.success());
+
+    let o1 = exec("--warnings=tests/data/easter-egg-errors-kt/verbose.log.gz");
+    assert!(o1.status.success());
+
+    let o2 = exec("--warnings=tests/data/easter-egg-errors-java/verbose.log.gz");
+    assert!(o2.status.success());
+
+    let o3 = exec("--warnings=tests/data/idmap-warnings/verbose.log.gz");
+    assert!(o3.status.success());
+
+    let o4 = exec("--warnings=tests/data/idmap-both-errors-and-warnings/verbose.log.gz");
+    assert!(o4.status.success());
+
+    let o5 = exec("--warnings=tests/data/idmap-errors/verbose.log.gz");
+    assert!(o5.status.success());
+}
